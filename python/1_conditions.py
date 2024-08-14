a0688f5f8c603946e5c27a0e95b1fe96e01ae074
farman.kaziakhmedov <farman.kaziakhmedov@gmail.com>
2024-08-14 22:07:12 +0400

    Updated docs

---
+++ python/1_conditions.py
@@ -0,0 +1,10 @@
+def launch():
+    n = int(input("Введи число: "))
+    if n % 2 == 0:
+        print("Hello World!")
+    else:
+        print("Goodbye world!")
+
+
+if __name__ == '__main__':
+    launch()
