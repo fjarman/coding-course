3072a4cbcdd0ce7170e7425ca3a2d0ac01f54709
farman.kaziakhmedov <farman.kaziakhmedov@gmail.com>
2024-08-19 00:02:51 +0400

    Changes

--- python_lang/sum_of_digits/solution.py
+++ python_lang/sum_of_digits/solution.py
@@ -4,8 +4,8 @@ def digital_root(n):
     while n > 10 or n == 10:
         counter = 0
         string = str(n)
-        com = len(string)
-        for i in range(com):
+        count = len(string)
+        for i in range(count):
             counter += int(string[i])
         n = counter
         print(n)
@@ -15,3 +15,4 @@ class TestStringMethods(unittest.TestCase):
 
     def test_correct(self):
         self.assertEqual(digital_root(435439), 1)
+        self.assertEqual(digital_root(1535), 4)
