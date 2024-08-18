c634fe47bfc50e9911ddcdd8b48efad59f05dafe
farman.kaziakhmedov <farman.kaziakhmedov@gmail.com>
2024-08-18 23:48:58 +0400

    First dev commit

--- python_lang/sum_of_digits/solution.py
+++ python_lang/sum_of_digits/solution.py
@@ -1,2 +1,17 @@
+import unittest
+
 def digital_root(n):
-    pass
+    while n > 10 or n == 10:
+        counter = 0
+        string = str(n)
+        com = len(string)
+        for i in range(com):
+            counter += int(string[i])
+        n = counter
+        print(n)
+    return n
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_correct(self):
+        self.assertEqual(digital_root(435439), 1)
