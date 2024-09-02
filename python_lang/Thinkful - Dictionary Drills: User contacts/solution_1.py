e9b42edaebbd42924521e15cc947612002275240
mrgraimon <sultanov.s.n@edu.mirea.ru>
2024-09-02 11:13:55 +0300

    Рабочее решение к задаче Thinkful - Dictionary Drills: User contacts

---
+++ python_lang/Thinkful - Dictionary Drills: User contacts/solution_1.py
@@ -0,0 +1,16 @@
+import unittest
+
+def user_contacts(data):
+    json = {}
+    for i in range(len(data)):
+        if len(data[i]) == 1:
+            json[data[i][0]] = None
+        else:
+            for j in range(1):
+                json[data[i][j]] = data[i][j+1]
+    return json
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_correct(self):
+        self.assert_equals(user_contacts([["Grae Drake", 98110], ["Bethany Kok"], ["Alex Nussbacher", 94101], ["Darrell Silver", 11201]]), {'Grae Drake': 98110, 'Darrell Silver': 11201, 'Alex Nussbacher': 94101, 'Bethany Kok': None})
