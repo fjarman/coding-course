4196b45ce092a10e734797338db03331d463634d
farman.kaziakhmedov <farman.kaziakhmedov@gmail.com>
2024-10-05 03:12:44 +0400

    Initial version of deployer

--- goals_app/deployment_system/deployer.py
+++ goals_app/deployment_system/deployer.py
@@ -20,7 +20,7 @@ def setup_logging():
     app.logger.addHandler(handler)
 
 
-@app.route("/deployer/welcome_test")
+@app.route("/deployer/welcome_test_2")
 def welcome():
     app.logger.info(os.environ.get('WERKZEUG_RUN_MAIN'))
     app.logger.info(request.form)
