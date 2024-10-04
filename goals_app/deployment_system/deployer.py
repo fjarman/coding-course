2481d08676973dc89add591a6d28b771781390d3
farman.kaziakhmedov <farman.kaziakhmedov@gmail.com>
2024-10-05 01:32:57 +0400

    Initial version of deployer

--- goals_app/deployment_system/deployer.py
+++ goals_app/deployment_system/deployer.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+import logging
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/deployer/deploy")
-def hello_world():
+# Setup logging
+def setup_logging():
+    handler = RotatingFileHandler('app.log', maxBytes=10000000, backupCount=3)
+    handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    app.logger.addHandler(handler)
+
+
+@app.route("/deployer/welcome")
+def welcome():
+    logging.info(request.form)
     return render_template('index.html')
+
+
+@app.route("/deployer/deploy")
+def deploy():
+    logging.info(request.form)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    setup_logging()
